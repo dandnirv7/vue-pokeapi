aa66c8a8e835e042ae53f734029ffad4e1daf7ab
dandnirv7 <dandnirv7@gmail.com>
2023-08-15 14:22:44 +0700

    feat: (+) InfoPokemon

---
+++ src/components/InfoPokemon.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div
+      class="flex flex-col items-center justify-center w-24 h-20 gap-1 px-2 py-3 rounded-md shadow-lg bg-black/40 shadow-black/50"
+    >
+      <h2 class="font-semibold">{{ title }}</h2>
+      <span class="text-sm capitalize"> {{ value }} </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    value: String,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
