da1c39bec52e6439f7b9d8bbe00e96ec43e5deb9
dandnirv7 <dandnirv7@gmail.com>
2023-08-16 14:32:27 +0700

    refactor: (+) font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      outfit: ["Outfit", "sans-serif"],
+      sans: ["Helvetica", "Arial", "sans-serif"],
+    },
   },
   plugins: [],
-}
-
+};
